52a228d5bf4dc5e6976d10399d82237c0aa4b867
Sagar Mehta <sagarmehta@Sagars.local>
2016-02-20 14:13:59 -0600

    error on row()
    working on it

---
+++ server_DB.js
@@ -0,0 +1,19 @@
+var sqlite3 = require('sqlite3');
+
+var db = new sqlite3.Database('blog.sqlite3')
+
+db.serialize (function() {
+db.run("CREATE TABLE if not exists Post (postId INTEGER PRIMARY KEY, title VARCHAR(25) NOT NULL, body VARCHAR(500) NOT NULL)");
+		 //CREATE TABLE Comment (comntId INTEGER PRIMARY KEY, body VARCHAR(500) PRIMARY KEY FOREIGN KEY REFERENCES Post(postId)); ")
+
+for(var i=0;i<20;i++){
+
+db.run("INSERT INTO Post (postId, title, body) VALUES (i, 'Title +i+','Body of post '+i+')");
+
+	db.each("SELECT * from Post", function(err, row(){
+		if(err) return console.log(row);
+		console.log(row);
+	})
+  }
+  
+});
